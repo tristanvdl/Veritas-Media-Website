609741d0e9014aac52d676e0a225111d199f1406
Leroy Manshande <Leroy Manshande>
2016-09-14 11:59:15 +0200

    menu links working

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,3 +1,37 @@
 $(document).ready(function(){
 
+  var menu = $('.ontop');
+
+  $(window).scroll(function () {
+    var y = $(this).scrollTop();
+    var z = $('.scrolltrigger').offset().top - 60;
+
+    if (y > z) {
+        //this triggers if you scroll past header
+        menu.removeClass('opbg').addClass('fullbg').addClass('blackborder');
+        // alert("awdawd");
+    }
+    else{
+        menu.removeClass('fullbg').addClass('opbg');
+    }
+  });
+
+  ///////////////////
+  function scrollToElement(link,dest){
+    $(link).click(function() {
+      $('html, body').animate({
+          scrollTop: $(dest).offset().top - 56
+      }, 750);
+    });
+  }
+
+  scrollToElement("#portfoliolink", "#portfolio");
+  scrollToElement("#aboutlink","#about");
+  scrollToElement("#logolink","#header");
+  //scrollToElement("#whatwedolink","#");
+  //scrollToElement("#contactlink","#");
+
+
+
+
 });
