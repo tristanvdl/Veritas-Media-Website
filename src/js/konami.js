c2aa5596cc5f81e3d2a0dd871186d55e047b2cb2
Leroy Manshande <Leroy Manshande>
2016-09-14 15:50:26 +0200

    contact stuff added

--- src/js/konami.js
+++ src/js/konami.js
@@ -38,6 +38,10 @@ function activateCheats() {
   var body = $('body');
   body.addClass('fa-spin');
   var audio = new Audio('konami.mp3');
+  audio.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
   audio.play();
 
   // alert("cheats activated");
