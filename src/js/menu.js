c16936832feb0f5ab0b06934a723a23fa3f1ed44
Tristan van der Linden <tristanvdl@gmail.com>
2016-09-15 13:47:23 +0200

    minor fixes

--- src/js/menu.js
+++ src/js/menu.js
@@ -36,7 +36,7 @@ $(document).ready(function(){
     function scrollToElement(link,dest){
         $(link).click(function() {
             $('html, body').animate({
-                scrollTop: $(dest).offset().top - 70
+                scrollTop: $(dest).offset().top - 50
             }, 750);
         });
     }
