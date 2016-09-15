b766438392e5fc8b52f3e221907bb85759853455
Tristan van der Linden <tristanvdl@gmail.com>
2016-09-15 09:29:04 +0200

    added actives to the menu and switched the position of what we do and who we are

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,32 +1,51 @@
 $(document).ready(function(){
 
-  var menu = $('.ontop');
+    var menu = $('.ontop');
 
-  $(window).scroll(function () {
-    var y = $(this).scrollTop();
-    var z = $('.scrolltrigger').offset().top - 72;
+    $(window).scroll(function () {
+        var y = $(this).scrollTop();
+        var z = $('.portfolio_trigger').offset().top - 72;
 
-    if (y > z) {
-        menu.removeClass('opbg').addClass('fullbg');
-    }
-    else{
-        menu.removeClass('fullbg').addClass('opbg');
-    }
-  });
+        if (y > z) {
+            menu.removeClass('opbg').addClass('fullbg');
+        }
+        else{
+            menu.removeClass('fullbg').addClass('opbg');
+        }
 
-  function scrollToElement(link,dest){
-    $(link).click(function() {
-      $('html, body').animate({
-          scrollTop: $(dest).offset().top - 70
-      }, 750);
+        function setClass(trigger, trigger2, menuitem) {
+            var scroll = $(this).scrollTop();
+            var elpos = $(trigger).offset().top - 72;
+            var nextel = $(trigger2).offset().top - 80;
+            var elmenu = $(menuitem);
+
+            if (scroll > elpos && scroll < nextel) {
+                elmenu.addClass('active_link');
+            }else {
+                elmenu.removeClass('active_link')
+            }
+        }
+
+        setClass('.portfolio_trigger','.what_trigger','.portfolio_item');
+        setClass('.what_trigger','.about_trigger','.what_item');
+        setClass('.about_trigger','.contact_trigger','.about_item');
+
     });
-  }
 
-  scrollToElement("#portfoliolink", "#portfolio");
-  scrollToElement("#aboutlink","#about");
-  scrollToElement("#logolink","#header");
-  //scrollToElement("#whatwedolink","#");
-  scrollToElement("#contactlink","#contact");
+    function scrollToElement(link,dest){
+        $(link).click(function() {
+            $('html, body').animate({
+                scrollTop: $(dest).offset().top - 70
+            }, 750);
+        });
+    }
+
+
+    scrollToElement("#portfoliolink", "#portfolio");
+    scrollToElement("#aboutlink","#about");
+    scrollToElement("#logolink","#header");
+    scrollToElement("#whatwedolink","#what");
+    scrollToElement("#contactlink","#contact");
 
 
 
